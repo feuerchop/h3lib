1b1b0e731cd39c9c6bb0ce74c8682929026de1b6
Xiao, Huang <xiaohu@in.tum.de>
2017-09-03 17:16:06 +0200

    clean and further maintain

--- mlcore/setup.py
+++ mlcore/setup.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python
 
+from distutils.core import setup
+
+setup(
+    name='h3mlcore',
+    version='0.1dev',
+    url='https://github.com/feuerchop/h3lib/tree/master/mlcore'
+    packages=['mlcore',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
+
+
 
